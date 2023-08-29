c31f58d11a96c292793c477471e5426404ffad1e
Jacob Sendzik <jake.sendzik@gmail.com>
2023-08-29 15:41:31 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+// Code your solution in this file
+const returnFirstTwoDrivers = (function(drivers) {
+    return [drivers[0], drivers[1]]
+})
+const returnLastTwoDrivers = (function(drivers) {
+    return [drivers[2], drivers[3]]
+})
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (num) => (fare) => num * fare
+
+const fareDoubler = (fare) => fare * 2
+
+const fareTripler = (fare) => fare * 3
+
+function selectDifferentDrivers(names, fn) {
+    return fn(names)
+}
